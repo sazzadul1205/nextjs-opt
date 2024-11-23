492d42f073c2668c4e1205356d1b371e596d72d0
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2024-11-23 15:28:28 +0600

    Caching and Revalidating NextJs

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -5,10 +5,20 @@ export const metadata = {
   description: "A Super Power full NextJS Website - About Page  ",
 };
 
-const AboutPage = () => {
+export const GetTime = async () => {
+  const res = await fetch(`http://localhost:3000/Time`, {
+    next: { revalidate: 5 },
+  });
+  const data = await res.json();
+  return data.currentTime;
+};
+
+const AboutPage = async () => {
+  const currentTime = await GetTime();
   return (
     <div>
       <p className="text-center text-3xl font-bold pt-5">About Page</p>
+      <p className="text-center text-3xl font-bold pt-5">Time: {currentTime}</p>
     </div>
   );
 };
