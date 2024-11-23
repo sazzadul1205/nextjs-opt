d5d2ba57c8a608a3c8d0d7d11af9a7ce681da32f
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2024-11-23 15:32:22 +0600

    Loader part

---
+++ src/app/Posts/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='text-center text-5xl mt-24 font-bold'>
+            <p>Data is Loading .. . .. . . . </p>
+        </div>
+    );
+};
+
+export default loading;
